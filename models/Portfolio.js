0ca9799ae5183886518b145243f75b9feaaf4777
joesphchang <joesphschang@gmail.com>
2022-03-10 12:00:58 -0500

    Completed my schema model

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -3,11 +3,17 @@ const mongoose = require('../db/connection');
 const PortfolioSchema = new mongoose.Schema({
 	title: String,
 	image: String,
-	description: String,
-	created: String,
-	deployed: String,
-	deployed_site: String,
-	github: String,
+    projects: {
+        title: String,
+        description: String,
+        photo: String,
+        photo_1: String,
+        photo_2: String,
+        photo_3: String,
+        photo_4: String,
+        photo_5: String,
+        photo_6: String,
+    },
 });
 
 const Portfolio = mongoose.model('Portfolio', PortfolioSchema);
