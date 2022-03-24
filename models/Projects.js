1f32927fcf8705c520e3161f62b23a6398a8aff9
joesphchang <joesphschang@gmail.com>
2022-03-24 13:19:37 -0400

    updated seed data

--- models/Projects.js
+++ models/Projects.js
@@ -4,14 +4,16 @@ const ProjectsSchema = new mongoose.Schema({
 	title: String,
 	description: String,
 	photo: String,
-	photo_1: String,
-	photo_2: String,
-	photo_3: String,
-	photo_4: String,
-	photo_5: String,
-	photo_6: String,
-	instagram: String,
-	website: String,
+	project_folder: {
+		photo_1: String,
+		photo_2: String,
+		photo_3: String,
+		photo_4: String,
+		photo_5: String,
+		photo_6: String,
+		instagram: String,
+		website: String,
+	},
 });
 
 const Projects = mongoose.model('Projects', ProjectsSchema);
